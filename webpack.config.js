4f1c65e9d67aa5e1cc99d638b1accb4ecf26e77a
Nicholas Pettas <npettas@gmail.com>
2018-05-03 11:31:09 -0700

    modify entry point to dist/

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,19 @@
- const path = require('path');
+const path = require('path');
+const webpack = require('webpack');
 
- module.exports = {
-     mode: 'production',
-     performance: { hints: false },
-     entry: './src/EmailToEthereumAddress.js',
-     output: {
-    path: path.resolve(__dirname, 'dist'),
-         filename: 'EmailToEthereumAddress.js'
-     }
- };
+module.exports = {
+    mode: 'production',
+    performance: {hints: false},
+    entry: './src/EmailToEthereumAddress.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'EmailToEthereumAddress.js',
+        libraryTarget: 'var',
+        library: 'EmailToEthereumAddress'
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            EmailToEthereumAddress: 'EmailToEthereumAddress'
+        })
+    ]
+};
